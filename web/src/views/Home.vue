d1c39e14efbd8f5338925e42dc73480bffdc8aeb
cedric <cedric.de.pauw@be-mobile.com>
2020-11-26 10:57:16 +0100

    Init

---
+++ web/src/views/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="home-container">
+    <div>
+      <h1 class="home-title">Let's get rid of your free time.</h1>
+      <h2 class="home-subtitle">Choose one:</h2>
+    </div>
+    <div class="picker-container">
+      <div class="picker-card">1</div>
+      <div class="picker-card">2</div>
+      <div class="picker-card">3</div>
+      <div class="picker-card">4</div>
+    </div>
+    <div>buttons</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.home-container {
+  display: flex;
+  align-items: left;
+  justify-content: flex-start;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #e1e6fc;
+  padding-left: 3em;
+  padding-right: 3em;
+}
+
+.home-title {
+  font-size: 2em;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-weight: bold;
+  text-align: left;
+}
+
+.home-subtitle {
+  font-size: 1em;
+  font-weight: bold;
+
+  text-align: left;
+}
+
+.picker-container {
+  display: grid;
+  grid-template: repeat(2, 1fr) / repeat(2, 1fr);
+  row-gap: 1em;
+  column-gap: 1em;
+  padding: 2em 0 1em 0;
+  height: 60%;
+  width: 100%;
+}
+
+.picker-card {
+  background-color: lightsteelblue;
+  border: 1px solid black;
+  border-radius: 5%;
+}
+</style>
---
+++ web/src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login-container">
+    <h1 class="login-title">Free time fixer</h1>
+    <div class="login">
+      <b-field label="Username">
+        <b-input icon="account"></b-input>
+      </b-field>
+
+      <b-field label="Password">
+        <b-input icon="key-variant" type="password" password-reveal> </b-input>
+      </b-field>
+      <div class="check-keep-logged-in">
+        <b-checkbox>Keep me logged in</b-checkbox>
+      </div>
+      <b-button tag="router-link" to="/home" type="is-link">
+        Log in
+      </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #fcf1e1;
+}
+
+.login {
+  width: 300px;
+}
+
+.login-title {
+  font-size: 4em;
+  padding: 1em;
+  font-weight: bold;
+}
+
+@media all and (max-device-width: 480px) {
+  .login-title {
+    font-size: 2em;
+    padding: 2em;
+    font-weight: bold;
+  }
+}
+</style>

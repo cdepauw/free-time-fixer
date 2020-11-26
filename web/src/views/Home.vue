88e138a6d91beaa7cf2b2fa835ca380f47c56899
cedric <cedric.de.pauw@be-mobile.com>
2020-11-26 17:11:50 +0100

    Tabs

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="main-container">
-    <div class="main-content">
-      <ActivityChooser v-if="State == 1" />
-      <ActivitySetup v-else-if="State == 0" />
-      <OngoingActivity v-else-if="State == 2" />
-    </div>
-    <Nav />
+    <b-tabs type="is-boxed" expanded>
+      <b-tab-item icon="flash" id="tabs">
+        <ActivityChooser v-if="State == 1" />
+        <ActivitySetup v-else-if="State == 0" />
+        <OngoingActivity v-else-if="State == 2" />
+      </b-tab-item>
+      <b-tab-item icon="format-list-bulleted"></b-tab-item>
+      <b-tab-item icon="logout-variant"></b-tab-item>
+    </b-tabs>
   </div>
 </template>
 
 <script>
-import Nav from "../components/Nav";
 import ActivityChooser from "../components/ActivityChooser";
 import ActivitySetup from "../components/ActivitySetup";
 import OngoingActivity from "../components/OngoingActivity";
@@ -18,31 +20,37 @@ import OngoingActivity from "../components/OngoingActivity";
 export default {
   name: "Home",
   components: {
-    Nav,
     ActivityChooser,
     ActivitySetup,
     OngoingActivity,
   },
   data() {
     return {
-      State: 2, // 0 = set-up, 1 = pick activity, 2 = ongoing activity
+      State: 1, // 0 = set-up, 1 = pick activity, 2 = ongoing activity
     };
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .main-container {
   display: flex;
-  align-items: left;
-  justify-content: flex-start;
-  flex-direction: column;
   width: 100%;
   height: 100%;
   background-color: #e1e6fc;
 }
 
-.main-content {
-  flex-grow: 1;
+.b-tabs.is-fullwidth {
+  height: 100%;
 }
+
+.b-tabs .tab-content {
+  height: 100%;
+}
+
+.b-tabs .tab-content .tab-item {
+  height: 100%;
+}
+
+
 </style>
